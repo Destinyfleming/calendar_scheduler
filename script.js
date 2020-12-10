306bc1128d40960301776d9302f070628bd23ff4
Destiny Fleming <destiny.fleming7401@gmail.com>
2020-12-10 14:17:09 -0800

    added into js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 
+// getting the date and time and adding that to the html text
+var todaysDate = moment().format("MMM Do YY"); 
+$('.currentDay').text(todaysDate);  
+
+var userEvent = $("#user-input").val().trim();
+// event listener for the save buttons
 $(".saveBtn").click(function() {
+// save user input into local storage
+localStorage.setItem("event", userEvent)
+alert(localStorage.getItem("event"));
     alert( "btn click" );
-    
-  });
+});
