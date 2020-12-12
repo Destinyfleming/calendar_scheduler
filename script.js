1a87224465df31d844eb373b0ea5eb4706053c81
Destiny Fleming <destiny.fleming7401@gmail.com>
2020-12-11 17:35:35 -0800

    created rows and coloms in js

--- script.js
+++ script.js
@@ -1,53 +1,49 @@
+var timeBlocks = $(".container");
+var btn = document.querySelectorAll("button");
 
 // getting the date and time and adding that to the html text
 var todaysDate = moment().format("MMM Do YY"); 
 $(".currentDay").text(todaysDate);  
 
-//save what the user entered into local storage
-//var userEvent = $("#user-input").val().trim();
-//localStorage.setItem("event", JSON.stringify(userEvent));
-//console.log(localStorage.getItem("event"));
-
-//getting the hour for future use
-var currentHour= moment().format("H");
-console.log(currentHour)
-
-// event listener for the save buttons
-$(".saveBtn").click(function(event) {
-    event.preventDefault();
-    alert("btn click");
-});
+//make arrays to reference diffent hours of the day 
+var dayHour = ["8 AM","9 AM","10 AM","11 AM","12 AM","1 PM","2 PM","3 PM","4 PM","5 PM"];
+var hourID = ["8","9","10","11","12","13","14","15","16","17"];
 
 //create rows and coloms dynamicly to manipulate
-var dayHour = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17" ]
-var hoursDiv = document.getElementById("time-block");
-dayHour.forEach(function(hour) {
+for (var i = 0; i < dayHour.length; i++){
+    var newTimeBlock = $("<div class='row time-block'>").attr("id", hourID[i]);
+    var newCol = $("<div class='hour col-1'>")
+    timeBlocks.append(newTimeBlock);
+    newCol.text(dayHour[i]);
+    newTimeBlock.append(newCol);
 
-    // For each drink in the array, we create a new paragraph to hold that text.
-    // A new paragraph will be created with each iteration of the loop.
-    var newHourP = document.createElement("p");
+    var textCol = $("<textarea class='col-10'>");
+    textCol.text();
+    newTimeBlock.append(textCol);
 
-    // We then assign the the text of this paragraph to be the text in the array item.
-    newHourP.textContent = hour;
+    var newBtn = $("<button type='button' class='saveBtn col-1 far fa-save'>");
+    newBtn.text();
+    newTimeBlock.append(newBtn);
+}
 
-    // We then add the paragraph to the our main div on the page ("#drink-options")
-    hoursDiv.appendChild(newHourP);
-  });
+//save what the user entered into local storage
 
 
 
 //make a function to change the colors based on past, present, future
-function currentTimeblock() {
+function hourColorFunction() {
+    var hourNow = moment().hours();
     $(".time-block").each(function() {
-        var blockHour = parseInt($(this).attr("id").split(" ")[0]);
-        if (blockHour < currentHour) {
-            $(this).addClass("future");
-        } else if (blockHour === currentHour) {
+        var scheduleHour = parseInt($(this).attr("id").split(" ")[0]);
+
+        if (scheduleHour < hourNow) {
+            $(this).addClass("past");
+        } else if (scheduleHour === hourNow) {
             $(this).addClass("present");
         } else {
-            $(this).addClass("past");
+            $(this).addClass("future");
         }
     });
 }
 
-currentTimeblock();
+hourColorFunction();
